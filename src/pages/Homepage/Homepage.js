957f77b89e4cdb53ddb5efd342a4298e65665f7f
Andy134 <Hong461995>
2021-01-21 02:40:03 +0700

    commit

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,12 +1,13 @@
+import { Container, Grid } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
+import ImgMediaCard from "./ImgMediaCard";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     root: {
-        minWidth: 275,
-        marginTop: '1rem'
+        marginTop: '0.5rem'
     },
     bullet: {
         display: 'inline-block',
@@ -19,17 +20,59 @@ const useStyles = makeStyles({
     pos: {
         marginBottom: 12,
     },
-});
+    control: {
+        padding: theme.spacing(2),
+    }
+}));
+
+const lstPost = [
+    {
+        title: "Lorem Ipsum is simply dummy",
+        img: "https://i.pinimg.com/564x/a3/12/8d/a3128d4ad2eb6a7d117d65f71afb3e78.jpg",
+        secondary: "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    },
+    {
+        title: "Lorem Ipsum is simply dummy",
+        img: "https://i.pinimg.com/564x/5d/5b/26/5d5b263d52e8d47023e9e3bf5f1be452.jpg",
+        secondary: "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    },
+    {
+        title: "Lorem Ipsum is simply dummy",
+        img: "https://i.pinimg.com/564x/84/13/d1/8413d12235faeb3775df846cf7303dca.jpg",
+        secondary: "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    },
+    {
+        title: "Lorem Ipsum is simply dummy",
+        img: "https://i.pinimg.com/564x/03/9a/45/039a4591e0e59dd7b5da0669f9424de9.jpg",
+        secondary: "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    }
+    ,
+    {
+        title: "Lorem Ipsum is simply dummy",
+        img: "https://i.pinimg.com/564x/98/a0/a3/98a0a37762fc82e7c8de7fa3dca28b9a.jpg",
+        secondary: "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    }
+    ,
+    {
+        title: "Lorem Ipsum is simply dummy",
+        img: "https://www.rolecostume.com/blog/wp-content/uploads/2015/06/gq0526ww06s.jpg",
+        secondary: "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    }
+]
 
 function Homepage() {
     const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
 
     return (
-        <Card className={classes.root} >
-            <CardContent>
-            </CardContent>
-        </Card>
+        <Container maxWidth="lg">
+            <Grid container spacing={2} className={classes.root}>
+                {
+                    lstPost.map((post, index) => {
+                        return <ImgMediaCard ket={index} post={post}/>
+                    })
+                }
+            </Grid>
+        </Container>
     )
 }
 
---
+++ src/pages/Homepage/ImgMediaCard.js
@@ -0,0 +1,67 @@
+import { Grid } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ShareIcon from '@material-ui/icons/Share';
+import React from 'react';
+
+const useStyles = makeStyles((theme) => ({
+    pullRight: {
+        marginLeft: 'auto'
+    },
+    cardIcon: {
+        fontSize: "medium"
+    },
+    title: {
+        lineHeight: "1.35em"
+    }
+}));
+
+export default function ImgMediaCard(props) {
+
+    const classes = useStyles();
+    const { title, img, secondary } = props.post;
+
+    return (
+        <Grid item lg={2} md={3} sm={4} xs={6}>
+            <Card >
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        alt={title}
+                        height="200"
+                        image={img}
+                        title={title}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h6" component="h6" className={classes.title}>
+                            {title}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {secondary}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions disableSpacing>
+                    <IconButton aria-label="add to favorites">
+                        <FavoriteIcon className={classes.cardIcon}/>
+                    </IconButton>
+                    <IconButton aria-label="share">
+                        <ShareIcon className={classes.cardIcon}/>
+                    </IconButton>
+                    <IconButton aria-label="settings" className={classes.pullRight}>
+                        <MoreVertIcon className={classes.cardIcon}/>
+                    </IconButton>
+                </CardActions>
+            </Card>
+        </Grid>
+    );
+}
